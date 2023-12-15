5b1ad4f85e48554ceec1ae045099ec2b5da58a1e
Zegert Boele <contact@zegert.nl>
2023-12-15 16:04:07 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zegert Boele</title>
+    <meta name="description" content="Zegert Boele - Webdevelopers personal portfolio.">
+    <link rel='shortcut icon' href='./favicon.ico' type='image/x-icon' />
+    <style>
+        @font-face {
+            font-display: optional;
+            font-family: 'Montserrat';
+            font-style: normal;
+            font-weight: 300;
+            src: url('./fonts/montserrat-v25-latin-300.eot');
+            src:
+                url('./fonts/montserrat-v25-latin-300.eot?#iefix') format('embedded-opentype'),
+                url('./fonts/montserrat-v25-latin-300.woff2') format('woff2'),
+                url('./fonts/montserrat-v25-latin-300.woff') format('woff'),
+                url('./fonts/montserrat-v25-latin-300.ttf') format('truetype'),
+                url('./fonts/montserrat-v25-latin-300.svg#Montserrat') format('svg');
+        }
+
+        body {
+            font-family: 'Montserrat', 'Roboto', sans-serif;
+            background-color: #121212;
+            color: #eeeeee;
+            font-size: 17px;
+            line-height: 1.6;
+            max-width: 650px;
+            margin: 40px auto;
+        }
+
+        @media only screen and (max-width: 700px) {
+            body {
+                margin: 40px 20px;
+            }
+        }
+
+        .pink,
+        a {
+            color: #b9134f;
+            transition: 0.3s;
+        }
+
+        a:hover {
+            color: rgb(139, 134, 134);
+        }
+
+        h4 {
+            margin-left: 20px;
+        }
+
+        p {
+            margin-left: 40px;
+        }
+
+    </style>
+
+</head>
+
+<body>
+    <h3>Who is <span class="pink">Zegert Boele</span>?</h3>
+    <h4>
+        According to <span class="pink">ChatGPT</span>:
+    </h4>
+    <p>
+        I'm sorry, but I couldn't find any information about a person named "Zegert Boele." It's possible that the name
+        is misspelled or that this person is not well-known or public figure. Can you provide any additional context or
+        details about who Zegert Boele is or what they're known for?
+    </p>
+    <h4>
+        According to <span class="pink">Github Copilot</span>:
+    </h4>
+    <p>
+        Zegert Boele (born 1969) is a Dutch politician. He was a member of the House of Representatives from 2006 to
+        2010,
+        representing the Labour Party. He was a member of the Senate from 2010 to 2017, representing the Labour Party.
+        He was a member of the House of Representatives from 2017 to 2021, representing the Labour Party. He was a
+        member
+        of the Senate from 2021 to 2021, representing the Labour Party.
+    </p>
+    <h4>
+        According to <span class="pink">me</span>:
+    </h4>
+    <p>
+        Junior webdeveloper, who loves making functional things and work with people. Currently working at <a
+            href="https://educto.nl">Educto</a>. Writing code with CakePHP. Always learning new stuff. <br><br>
+
+        Busy riding my motorcycle, a Honda NC700s, and a lifeguard at the <a href="https://www.rbdordrecht.nl">Dordtse
+            Reddingsbrigade</a>.
+    </p>
+    <h4>Connect?</h4>
+    <p>
+        <a href="mailto:contact@zegert.nl">Email</a>
+        <a href="https://www.linkedin.com/in/ZegertBoele">LinkedIn</a>
+        <a href="https://github.com/Zegert">Github</a>
+        <a href="https://www.instagram.com/zegert_/">Instagram</a>
+
+    </p>
+</body>
+
+</html>
