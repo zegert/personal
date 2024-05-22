813875020d3b63230fb68f592ea075dc767660a1
Zegert Boele <contact@zegert.nl>
2024-05-22 16:18:11 +0200

    end

--- ics.php
+++ ics.php
@@ -23,6 +23,17 @@ $ical = "BEGIN:VCALENDAR <br>
         ORGANIZER;CN=Admin Educto:MAILTO:admin@educto.nl <br> 
         ATTENDEE;CN=Admin Educto:MAILTO:admin@educto.nl <br> 
         ATTENDEE;CN=Senna Kok:MAILTO:jolijn85@example.net <br> 
-        URL:http://localhost:8080/calendar/item/get/1 <br>";
+        URL:http://localhost:8080/calendar/item/get/1 <br> 
+        DTSTART:20240522T120000Z DTEND:20240521T143000Z <br> 
+        END:VEVENT BEGIN:VEVENT UID:664dfa791590a <br> 
+        DTSTAMP:20240522T140025Z <br> 
+        SUMMARY:test 123 test <br> 
+        DESCRIPTION:Lorem Ipsum is simply dummy text of the printing and typesettin g industry. Lorem Ipsum has been the industry's standard dummy text ever s ince the 1500s\, when an unknown printer took a galley of type and scrambl ed it to make a type specimen book. It has survived not only five centurie s\, but also the leap into electronic typesetting\, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sh eets containing Lorem Ipsum passages\, and more recently with desktop publ ishing software like Aldus PageMaker including versions of Lorem Ipsum. <br> 
+        LOCATION:Teams meeting zie beschrijving <br> 
+        ORGANIZER;CN=Admin Educto:MAILTO:admin@educto.nl <br> 
+        ATTENDEE;CN=Admin Educto:MAILTO:admin@educto.nl <br> 
+        URL:http://localhost:8080/calendar/item/get/2 <br> 
+        END:VEVENT <br> 
+        END:VCALENDAR";
 
 echo nl2br($ical);
